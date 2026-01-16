268568e20e82e813bd1f7250dd74a1e59a4e2e58
Rafael Montero <developer@rmontero.me>
2026-01-16 16:09:59 +0400

    Error Handlers with DRY principle
    No longer needed because of native support from Express 5

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,59 +1,44 @@
 const Bootcamp = require('../models/Bootcamp');
+const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/async') // No longer needed because of native support from Express 5
 
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
 exports.getBootcamps = async (req, res, next) => {
-    try {
-        const bootcamps = await Bootcamp.find();
-        res.status(200).json({success: true, data: bootcamps});
-    } catch (err) {
-        next(err);
-    }
-    
+    const bootcamps = await Bootcamp.find();
+    res.status(200).json({success: true, data: bootcamps});
 }
 // @desc    Get single bootcamp
 // @route   GET /api/v1/bootcamps/:id
 // @access  Private
 exports.getBootcamp = async (req, res, next) => {
 
-    try {
-        const bootcamp = await Bootcamp.findById(req.params.id);
+    const bootcamp = await Bootcamp.findById(req.params.id);
 
-        if (!bootcamp) {
-            next(new ErrorResponse(`Bootcamps of ${req.params.id} not found`, 404));
-        }
-        res.status(200).json({ success: true, data: bootcamp });
-    } catch (err) {
-        next(err);
+    if (!bootcamp) {
+        next(new ErrorResponse(`Bootcamps of ${req.params.id} not found`, 404));
     }
+    res.status(200).json({ success: true, data: bootcamp });
 }
 // @desc    Create single bootcamp
 // @route   POST /api/v1/bootcamps
 // @access  Private
 exports.createBootcamp = async (req, res, next) => {
-    try {
-        const bootcamp = await Bootcamp.create(req.body);
-        res.status(201).json({success: true, data: bootcamp});
-    } catch (err) {
-       next(err);
-    }
+    const bootcamp = await Bootcamp.create(req.body);
+    res.status(201).json({success: true, data: bootcamp});
 
 }
 // @desc    Update single bootcamp
 // @route   PUT /api/v1/bootcamps/:id
 // @access  Private
 exports.updateBootcamp = async (req, res, next) => {
-    try {
-        const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
-            new: true,
-            runValidators: true
-        });
+    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    });
         res.status(200).json({success: true, data: bootcamp});
     if (!bootcamp) {
         next(new ErrorResponse(`Bootcamps of ${req.params.id} not found`, 404));
-    }
-    } catch (error) {
-        next(err);
     }
 }
