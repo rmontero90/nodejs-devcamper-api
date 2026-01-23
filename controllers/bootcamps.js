982d8a6fa3576489b828e019d178ad30d3ba7c46
Rafael Montero <developer@rmontero.me>
2026-01-23 07:59:21 -0600

    Enhance bootcamp query filtering, sorting, and field selection
    Advanced filtering, field selection, and sorting based on query parameters. Aadded support for MongoDB operators.

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -9,10 +9,40 @@ const geocoder = require('../utils/geocoder');
 exports.getBootcamps = async (req, res, next) => {
     
     let query;
-    let queryStr = JSON.stringify(req.query);
-    queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
-    query = await Bootcamp.find(JSON.parse(queryStr));
+
+    //Copy req.query
+    const reqQuery = { ...req.query };
+    // Fields to exclude
+    const removeFields = ['select', 'sort', 'page', 'limit'];
+
+    //Loop over removeFields and delete them from reqQuery
+    removeFields.forEach(param => delete reqQuery[param]);
+
+    console.log(reqQuery);
+
+    let queryStr = JSON.stringify(reqQuery);
     
+    //Create operators ($gt, $gte, etc)
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+    
+    //Finding resources
+    query =  Bootcamp.find(JSON.parse(queryStr));
+    
+    //Select Fields
+    if (req.query.select) {
+        const fields = req.query.select.split(',').join(' ');
+        query = query.select(fields);
+    }
+
+    // Sorting
+    if (req.query.sort) {
+        const sortBy = req.query.sort.split(',').join(' ');
+        query = query.sort(sortBy);
+    } else {
+        query = query.sort('-createdAt');
+    }
+    
+    //Executing query
     const bootcamps = await query;
     res.status(200).json({success: true, count: bootcamps.length, data: bootcamps});
 }
