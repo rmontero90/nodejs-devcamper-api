c7dcf2ff61e4dea950caaaaab05125a5f1d121b9
Rafael Montero <developer@rmontero.me>
2026-03-09 20:40:33 +0300

    Add user auth: register, login, JWT & hashing
    - Implement user authentication: add register and login controllers that create and return JWTs, and wire up /register and /login routes. - Enhance User model with bcrypt password hashing (pre-save hook) and methods getSignedJwtToken and matchPassword using jsonwebtoken and bcryptjs.

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,10 +5,43 @@ const User = require('../models/User');
 // @route   POST /api/v1/auth/register
 // @access  Public
 exports.register = async (req, res, next) => {
-    // const { name, email, password, role } = req.body;
+    const { name, email, password, role } = req.body;
 
-    res.status(200).json({success: true});
-    // try {
-    //     const user = await User.create({ name, email, password, role });
-    // }
+    try {
+        const user = await User.create({ name, email, password, role });
+
+        // Create token
+        const token = user.getSignedJwtToken();
+        res.status(200).json({success: true, token});
+    } catch (err) {
+        next(new ErrorResponse(err.message.red), 500);
+    }
+}
+
+// @desc    Login user
+// @route Post /api/v1/auth/login
+// @access Public
+
+exports.login = async (req, res, next) => {
+    const { email, password }= req.body;
+
+    if (!email || !password) {
+        return next(new ErrorResponse('Please provide an email and password', 400));
+    }
+    // Check for user
+    const user = await User.findOne({ email }).select('+password');
+
+    if(!user) {
+        return next(new ErrorResponse('Invalid credentials', 401));
+    }
+    // Check if password matches
+    const isMatch = await user.matchPassword(password);
+
+    if(!isMatch) {
+        return next(new ErrorResponse('Invalid credentials', 401));
+    }
+    // Create token
+    const token = await user.getSignedJwtToken();
+    res.status(200).json({success: true, token});
+    
 }
