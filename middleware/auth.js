fdd05fb13f55536846dc00d09f2caa6f69408f19
Rafael Montero <developer@rmontero.me>
2026-03-10 18:10:27 +0300

    Refactor token extraction and remove log
    Use optional chaining for header access and add a cookie fallback for token retrieval (req.cookies.mytoken).

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,28 +6,23 @@ const User = require('../models/User');
 exports.protect = async (req, res, next) => {
     let token;
 
-    if (req.headers.authorization && 
-        req.headers.authorization.startsWith('Bearer')
-    ) {
+    if (req.headers.authorization?.startsWith('Bearer')) {
         token = req.headers.authorization.split(' ')[1];
-    } 
-    // else if (req.cookies.mytoken) {
-    //     token = req.cookies.mytoken
-    // }
-
-    // Make sure token exists
-    if (!token) {
-        return next (new ErrorResponse('Not authorized to access this route', 401));
     }
-    try {
-        // Verify token
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        console.log(decoded);
+    else if (req.cookies.mytoken) {
+        token = req.cookies.mytoken;
+    }
 
+    if (!token) {
+        return next(new ErrorResponse('Not authorized to access this route', 401));
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = await User.findById(decoded.id);
         next();
     } catch (err) {
-        return next (new ErrorResponse('Not authorized to access this route', 401));
+        return next(new ErrorResponse('Not authorized to access this route', 401));
     }
 }
 
